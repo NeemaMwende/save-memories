567c629ac7df9c79c12f4afc3ea4be25bb3f4baa
NeemaMwende <gracepete12@gmail.com>
2024-06-08 21:54:24 +0300

    updates

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import "./Main.css"
+
+const Main = () => {
+    return ( 
+        <div className="Main">
+            <h5>Create Memories That Never Die</h5>
+        </div>
+     );
+}
+ 
+export default Main;
